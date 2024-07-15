b53e20d2b4d194efe9272f9d85aad255d81967cd
Pleic <169063152+Fvcklyx@users.noreply.github.com>
2024-07-15 19:52:20 +0700

    Add files via upload

---
+++ Hutsmal/script.js
@@ -0,0 +1,5 @@
+// show list navbar
+document.getElementById("barsButton").addEventListener("click", showList);
+function showList() {
+  document.getElementById("hideNav").classList.toggle("visibleNav");
+}
